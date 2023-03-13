930b79260287d0ea265e2ca5287286f9e9a3574b
CWestICL <cwest@ic.ac.uk>
2023-03-13 09:21:58 +0000

    Vite app

--- sesquipedaily/frontend/src/App.js
+++ sesquipedaily/frontend/src/App.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import Axios from "axios";
 
 import Layout from "./hocs/Layout"
 
@@ -8,6 +9,7 @@ import Register from "./containers/Register"
 import Login from "./containers/Login"
 import Dashboard from "./containers/Dashboard"
 
+/*
 const App = () => (
     <Router>
         <Layout>
@@ -18,5 +20,49 @@ const App = () => (
         </Layout>
     </Router>
 );
+
+function App() {
+
+    const [users, setUsers] = useState("");
+
+    Axios.get("http://127.0.0.1:8000/accounts/users").then(
+        (response) => {
+            setUsers(response.data);
+        }
+    );
+
+    return (
+        <div>{users}</div>
+    );
+}
+
+*/
+
+const App = () => {
+    const [users, setUsers] = useState("");
+
+    async function fetchUsers() {
+        try {
+            //const response = await Axios.get("http://127.0.0.1:8000/accounts/users");
+            const response = await fetch("/accounts/users");
+            const usersData = await response.json();
+            setUsers(JSON.stringify(usersData));
+        } catch (err) {
+            console.log("Oh no an error! ", err)
+        }
+    }
+
+    useEffect(() => {
+        fetchUsers();
+    }, []);
+
+    return (
+        <main>
+            <h1>Users:</h1>
+            <p>{users}</p>
+        </main>
+    )
+};
+
 
 export default App;
